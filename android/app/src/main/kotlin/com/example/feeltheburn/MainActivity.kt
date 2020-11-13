e631811f78721e18a1839628b47de52b5644c6d3
mark-doblefilo <i92romom@uco.es>
2020-11-13 13:13:29 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/feeltheburn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feeltheburn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+/**
+  * Author: Marc Rodriguez
+  * Github: github.com/mark-doblefilo
+    */
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
