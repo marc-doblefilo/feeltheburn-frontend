2abfe0f004612042abd5a5a5f7b8e072edb4c45c
mark-doblefilo <i92romom@uco.es>
2020-11-13 13:21:22 +0100

    Added constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const BackgroundColor = Color(0xFFF8F8F8);
+const ActiveIconColor = Color(0xFFE68342);
+const TextColor = Color(0xFF222B45);
+const BlueLightColor = Color(0xFFC7B8F5);
+const BlueColor = Color(0xFF817DC0);
+const ShadowColor = Color(0xFFE6E6E6);
