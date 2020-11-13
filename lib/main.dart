e078d6f29fe83cdde2bccd973914dec171d13de2
mark-doblefilo <i92romom@uco.es>
2020-11-13 14:00:53 +0100

    Added MyApp()

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-/**
-  * Author: Marc Rodriguez
-  * Github: github.com/mark-doblefilo
-    */
-
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Feel The Burn',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Feel The Burn'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
